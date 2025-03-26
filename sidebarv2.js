245e960a9335253c197a4c9a2df773922815e8c3
Mocha The Raccoon <jackson.z.davis@icloud.com>
2025-03-26 14:06:40 +1100

    site start

---
+++ sidebarv2.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+// Project not accepting anonymous feedback
+
+}
+/*
+     FILE ARCHIVED ON 14:16:08 Apr 05, 2020 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 01:31:34 Mar 26, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.502
+  exclusion.robots: 0.018
+  exclusion.robots.policy: 0.009
+  esindex: 0.01
+  cdx.remote: 8.983
+  LoadShardBlock: 449.932 (3)
+  PetaboxLoader3.datanode: 357.016 (4)
+  PetaboxLoader3.resolve: 277.379 (2)
+  load_resource: 203.32
+*/
